b1d2e98561692b48c76ef5f1de098338be63021e
Madushan98 <ranasinhemadushan@gmail.com>
2022-11-17 18:29:53 +0530

    fix: read function error handle

--- partA.c
+++ partA.c
@@ -3,14 +3,12 @@
 #include <string.h> // for manipulating arrays of characters.
 #include <errno.h>  // for error handling
 
-
-
 void createRecord();
 void writeOneRecord();
 void readAllRecords();
 
-typedef struct student_marks    // structure definition for student marks
-{ 
+typedef struct student_marks // structure definition for student marks
+{
     char student_index[20];
     float assignmt01_marks;
     float assignmt02_marks;
@@ -21,12 +19,12 @@ typedef struct student_marks    // structure definition for student marks
 int main()
 {
     int choice;
-    printf("Module Code: ZZ6309 \n");
-    printf("Module Name: Linux Operating System \n\n");
+    printf("----Module Code: ZZ6309----- \n");
+    printf("----Module Name: Linux Operating System---- \n\n");
 
     do
     {
-
+        printf("\n ------Menu------ \n\n");
         printf("1. Create Record \n");
         printf("2. Read all Record \n");
         printf("3. Delete Record \n");
@@ -61,8 +59,7 @@ int main()
     return 0;
 }
 
-
-void createRecord()
+void createRecord() // function to create a record
 {
     student_marks input;
 
@@ -80,28 +77,34 @@ void createRecord()
     writeOneRecord(input);
 }
 
-void writeOneRecord(student_marks data)
+void writeOneRecord(student_marks data) // function to write a student record to file
 {
 
     FILE *file;
     int wrt;
     int errNo;
 
-    file = fopen("student_marks.dat", "a+");
-
-    wrt = fwrite(&data, sizeof(struct student_marks), 1, file);
-
-    if (wrt < 0)
+    file = fopen("student_marks.dat", "a+"); // open file in append mode
+    if (file == NULL)
     {
-        perror("fprintf doc1.txt: ");
-        printf("Error No: %d\n", errno);
+        errNo = errno;
+        printf("Error opening file: %s\n", strerror(errNo));
         exit(1);
     }
 
-    fclose(file);
+    wrt = fwrite(&data, sizeof(struct student_marks), 1, file); // write one record to file
+
+    if (wrt < 0) // check for error in write
+    {
+        printf("Error No: %d\n", errno);
+        perror("fwrite student_marks.dat: ");
+        exit(1);
+    }
+
+    fclose(file); // close file
 }
 
-void readAllRecords()
+void readAllRecords() // function to read all record on the student_marks file
 {
     student_marks output;
     FILE *file;
@@ -120,11 +123,21 @@ void readAllRecords()
 
     printf("| %*s | %*s | %*s | %*s | %*s | \n\n", -20, row1[0], -20, row1[1], -20, row1[2], -20, row1[3], -20, row1[4]);
 
-    while (fread(&output, sizeof(struct student_marks), 1, file))
+    while (!feof(file)) // read each record from file with error handling
     {
-           printf("| %-20s | %-20.2f | %-20.2f | %-20.2f | %-20.2f  | \n", output.student_index, output.assignmt01_marks,
-               output.assignmt02_marks, output.project_marks, output.finalExam_marks);
+        fread(&output, sizeof(struct student_marks), 1, file);
+        if ((errNo = ferror(file)) > 0)
+        {
+            perror("fread dox1.txt: ");
+            printf("ferror: %d\n", errNo);
+            exit(1);
+        }
+        else
+        {
+            printf("| %-20s | %-20.2f | %-20.2f | %-20.2f | %-20.2f  | \n", output.student_index, output.assignmt01_marks,
+                   output.assignmt02_marks, output.project_marks, output.finalExam_marks);              // print record
+        }
     }
     printf("\n\n");
-    fclose(file);
+    fclose(file); // close file
 }
