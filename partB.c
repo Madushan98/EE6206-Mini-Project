3daa71a417d59e987568b011cc8edaf43cf79ad1
Madushan98 <ranasinhemadushan@gmail.com>
2022-11-18 13:17:09 +0530

    feat: get_all_records function created

--- partB.c
+++ partB.c
@@ -25,11 +25,15 @@ typedef struct analize_marks // structure definition for analize_marks
     int numofstudent_below;
 } analize_marks;
 
+int getAllDataWithLength();
 
+student_marks *marksPtr;
 
 void main()
 {
-      key_t ky = ftok("file.txt", 78); // create a key for shared memory
+    int length = getAllDataWithLength(); // get all records and length of records from file
+
+    key_t ky = ftok("file.txt", 78); // create a key for shared memory
     if (ky == -1)
     {
         perror("ftok error: ");
@@ -54,12 +58,11 @@ void main()
     }
     else if (PID1 == 0)
     { // Child Process C1
-      
     }
     else
     {
 
-        pid_t PID2 = fork();    // create child process 2
+        pid_t PID2 = fork(); // create child process 2
 
         if (PID2 == -1)
         {
@@ -69,11 +72,10 @@ void main()
         }
         else if (PID2 == 0)
         { // Child Process C2
-           
         }
         else
         {
-            pid_t PID3 = fork();    // create child process 3
+            pid_t PID3 = fork(); // create child process 3
 
             if (PID3 == -1)
             {
@@ -83,13 +85,11 @@ void main()
             }
             else if (PID3 == 0)
             { // Child Process C3
-
-               
             }
             else
             {
 
-                pid_t PID4 = fork();    // create child process 4
+                pid_t PID4 = fork(); // create child process 4
 
                 if (PID4 == -1)
                 {
@@ -99,14 +99,42 @@ void main()
                 }
                 else if (PID4 == 0)
                 { // Child Process C4
-                   
                 }
                 else
                 { // Parent Process
-
-                
                 }
             }
         }
     }
+}
+
+int getAllDataWithLength() // function to get the number of records in the file
+{
+    student_marks output;
+    FILE *file;
+    int count = 0;
+
+    marksPtr = (student_marks *)malloc(count * sizeof(struct student_marks)); // allocate memory for the structure
+
+    file = fopen("student_marks.dat", "r"); // open file for writing and reading
+    if (file == NULL)                       // error handling
+    {
+        perror("doc1.txt: ");
+        printf("Error No %d\n", errno);
+        exit(1);
+    }
+    printf("\n");
+    while (fread(&output, sizeof(struct student_marks), 1, file)) // read each record from file with error handling
+    {
+        count++;
+        marksPtr = (student_marks *)realloc(marksPtr, count * sizeof(struct student_marks)); // reallocate memory for the structure
+        strcpy((marksPtr + count - 1)->student_index, output.student_index);                 // copy data to the structure
+        (marksPtr + count - 1)->assignmt01_marks = output.assignmt01_marks;
+        (marksPtr + count - 1)->assignmt02_marks = output.assignmt02_marks;
+        (marksPtr + count - 1)->project_marks = output.project_marks;
+        (marksPtr + count - 1)->finalExam_marks = output.finalExam_marks;
+    }
+
+    fclose(file); //  close file
+    return count;   // return the number of records
 }
